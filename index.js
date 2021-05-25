f7946ba3fa3355676f61752d552fae8a2e19703c
tomfinnegan <thomasjosephfinnegan@gmail.com>
2021-05-25 22:33:54 +0000

    test js

---
+++ index.js
@@ -0,0 +1,65 @@
+//console.log("ho");
+//alert gdwq.w;
+
+
+// variables
+
+//var b = "smoothie";
+//console.log(b);
+
+
+//var someNumber = 45;
+//console.log(someNumber);
+
+
+
+   // var age = prompt("what is you age");
+ //document.getElementById("someText").innerHTML = age;
+
+// numbers in jaavascript
+
+var num1 = 11;
+//num1++ ;
+//console.log(num1);
+//var num1 ;
+//num1-- ;
+console.log(num1);
+
+//divide multiply*, remander %; 
+
+//console.log(num1 / 6);
+
+// increment or decremt by anynumber you want;
+
+num1 +=20;
+console.log(num1);
+
+
+/*functions
+1. create a function
+2. call the function 
+*/
+//create
+function fun() {
+    console.log("this is a function");
+}
+
+//call
+fun();
+
+/* lets create a function that take in your name
+ and says hello followed by your name
+
+ for example 
+
+ name thomas
+ return " hello thomas"
+ */
+function greeting() {
+   var name = prompt("what is your name");
+   var result = "hello" + name;
+   console.log(result);
+}
+
+greeting();
+
