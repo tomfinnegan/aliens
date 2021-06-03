265b242d8373c11e18efb562d43eeb20c986f4f0
tomfinnegan <thomasjosephfinnegan@gmail.com>
2021-06-03 14:46:07 +0000

    added navbar to page

--- index.js
+++ index.js
@@ -15,3 +15,12 @@ loginButton.addEventListener("click", (e) => {
         loginErrorMsg.style.opacity = 1;
     }
 })
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
